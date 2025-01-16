331178fef1e482ff34fe2c638bb1f5557368d824
RhexNiebres <rhexniebres@gmail.com>
2025-01-15 20:30:15 -0800

    change entry from index to script.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Your entry point
+  entry: './src/script.js', // Your entry point
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
